8effbe2a6d08ca6e082df25589b3a35cace69b12
Lucas Montanari <lmontanari@csumb.edu>
2019-02-26 12:52:19 -0800

    added more to the level

--- SuperFunkyBall/Assets/Scripts/PlayerController.cs
+++ SuperFunkyBall/Assets/Scripts/PlayerController.cs
@@ -19,11 +19,19 @@ public class PlayerController : MonoBehaviour
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
         float h = Input.GetAxis("Horizontal") * rotateSpeed * Time.deltaTime;
-
+        bool jump = Input.GetButtonDown("Fire1");
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
         rb.AddForce(movement * speed);
         rb.AddTorque(transform.right * h);
+
+        if(jump)
+        {
+            Vector3 jumpForce = new Vector3(0.0f, 200.0f, 0.0f);
+            rb.AddForce(jumpForce);
+            Debug.Log("I should jump");
+        }
     }
+
 
 }
---
+++ SuperFunkyBall/Assets/Scripts/Win.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Win : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Time.timeScale = 0;
+            Debug.Log("YOU WIN!!");
+        }
+    }
+}
